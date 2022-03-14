15450c01f375d2802cd5d869d7755dbc73c5e849
mjassdev <matheusetf@gmail.com>
2022-03-13 21:59:44 -0300

    first commit

---
+++ Back/src/ProEventos.API/Controllers/EventoController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ProEventos.API.Models;
+
+namespace ProEventos.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventoController : ControllerBase
+    {
+
+        public IEnumerable<Evento> _evento = new Evento[]{
+                new Evento(){
+                    EventoId = 1,
+                    Tema = "Angular 11",
+                    Local = "Palmas-TO",
+                    Lote = "1º Lote",
+                    QtdPessoas = 200,
+                    DataEvento = DateTime.Now.AddDays(2).ToString()
+                },
+                new Evento(){
+                    EventoId = 2,
+                    Tema = "Angular 12",
+                    Local = "Araguaína-TO",
+                    Lote = "1º Lote",
+                    QtdPessoas = 350,
+                    DataEvento = DateTime.Now.AddDays(3).ToString()
+                }
+        };
+        public EventoController()
+        {
+
+        }
+
+        [HttpGet]
+        public IEnumerable<Evento> Get()
+        {
+            return _evento;
+        }
+
+        [HttpGet("{id}")]
+        public IEnumerable<Evento> GetPai(int id)
+        {
+            return _evento.Where(evento => evento.EventoId == id);
+        }
+    }
+}
